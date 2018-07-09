1a3308d8daee4debd4f6b18cd889efdb113ae19d
liuxiaopiao <liuxiaopiao@gmail.com>
2018-07-09 06:21:36 +0000

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example v0.1"
+	title := "Jenkins X golang http example v0.2"
 
 	from := ""
 	if r.URL != nil {
